43c89c53889b6120629f6ae3e250030c9d1eb455
Lluvian <dwayne@dontasweb.com>
2020-09-15 10:00:58 -0500

    Modified home html with custom cards and prevents large file uploads over 1MB.

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    //https://jira.spring.io/browse/SPR-14651
+    //Spring 4.3.5 supports RedirectAttributes
+    @ExceptionHandler(MultipartException.class)
+    public String handleError1(MultipartException e, RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addFlashAttribute("error", " Your request was rejected because the file size exceeds the configured maximum 1MB.");
+        return "redirect:/home";
+
+    }
+
+    /* Spring < 4.3.5
+	@ExceptionHandler(MultipartException.class)
+    public String handleError2(MultipartException e) {
+
+        return "redirect:/errorPage";
+
+    }*/
+
+}
